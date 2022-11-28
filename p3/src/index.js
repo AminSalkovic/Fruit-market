be3a2e92ee1f8bb5f11315fbbc7be947a1b56455
AminSalkovic <aminsalkovic2019@gmail.com>
2022-11-28 14:31:37 +0100

    amin

--- p3/src/index.js
+++ p3/src/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import {createRoot} from 'react-dom/client';
+import {createBrowserRouter,RouterProvider, Outlet} from 'react-router-dom'
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const AppLayout=()=>(
+  <>
+     <Navbar/>
+     <Outlet/>
+  </>
+  )
+
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<AppLayout/>,
+  
+  children:[
+    {
+      path:'/',
+      element:<Home/>
+    },
+    {
+      path:'about',
+      element:<About/>
+    }
+  ]
+}
+])
+ createRoot(document.getElementById('root')).render(
+   <RouterProvider router={router}/>
 );
 
